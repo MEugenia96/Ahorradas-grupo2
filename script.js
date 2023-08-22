b8e22e44de3e8705151cabc075ba630c866407da
Maria Eugenia O <eugeniaortiz162@gmail.com>
2023-08-22 20:43:00 -0300

    Funcionalidad de botones para la navegacion

---
+++ script.js
@@ -0,0 +1,41 @@
+const $ = (selector) => document.getElementById(selector);
+
+//Boton +nueva operacion
+$('button-operation').addEventListener('click', () => addNewOperation());
+
+const addNewOperation = () =>{
+    $('seccion-balance-y-filtro').classList.add('hidden');
+    $('seccion-operaciones').classList.add('hidden');
+    $('modal-nueva-operacion').classList.remove('hidden');
+};
+
+//Boton balance
+$('boton-balance').addEventListener('click', () => returnInicio());
+
+const returnInicio = () =>{
+    $('seccion-balance-y-filtro').classList.remove('hidden');
+    $('seccion-operaciones').classList.remove('hidden');
+    $('modal-nueva-operacion').classList.add('hidden');
+    $('seccion-reportes').classList.add('hidden');
+};
+
+//Boton categorias
+$('boton-categorias').addEventListener('click', () => categoryList());
+
+const categoryList = () =>{
+    $('seccion-balance-y-filtro').classList.add('hidden');
+    $('seccion-operaciones').classList.add('hidden');
+    $('modal-nueva-operacion').classList.add('hidden');
+    $('seccion-categorias').classList.remove('hidden');
+};
+
+//Boton reportes
+$('boton-reportes').addEventListener('click', () => viewReports());
+
+const viewReports = () =>{
+    $('seccion-balance-y-filtro').classList.add('hidden');
+    $('seccion-operaciones').classList.add('hidden');
+    $('modal-nueva-operacion').classList.add('hidden');
+    $('seccion-categorias').classList.add('hidden');
+    $('seccion-reportes').classList.remove('hidden');
+}
