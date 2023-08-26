88a039a2e96caa1c765a608723bb7ee9d2bc2657
Maria Eugenia O <eugeniaortiz162@gmail.com>
2023-08-26 14:15:46 -0300

    Captar info de los input para agregar nueva operacion

--- script.js
+++ script.js
@@ -39,3 +39,51 @@ const viewReports = () =>{
     $('seccion-categorias').classList.add('hidden');
     $('seccion-reportes').classList.remove('hidden');
 }
+
+//Agregar nuevas operaciones
+
+//array de objetos para pushear ej nuevasOperaciones.descripcion = valor
+const nuevasOperaciones = [
+    {}
+];
+
+//* Agregar descripcion *//
+$('descripcion').addEventListener('change', (e) => captarDescripcion(e));
+const captarDescripcion = (e) =>{
+    let vlrDescrip = e.target.value;
+    nuevasOperaciones[0].descripcion = vlrDescrip
+}
+//* Agregar monto *//
+$('monto').addEventListener('change', (e) => captarMonto(e));
+const captarMonto = (e) =>{
+    let vlrMonto = e.target.value;
+    nuevasOperaciones[0].monto = vlrMonto;
+}
+//* Agregar tipo *//
+$('tipo').addEventListener('change', (e) => captarTipo(e));
+const captarTipo = (e) =>{
+    let vlrTipo = e.target.value;
+    nuevasOperaciones[0].tipo = vlrTipo;
+}
+//* Agregar categoria *//
+$('select-categoria').addEventListener('change', (e) => captarCategoria(e));
+const captarCategoria = (e) =>{
+    let vlrCategoria = e.target.value;
+    nuevasOperaciones[0].categoria = vlrCategoria;
+}
+//* Agregar fecha *//
+$('fecha').addEventListener('change', (e) => captarFecha(e));
+const captarFecha = (e) =>{
+    let vlrFecha = e.target.value;
+    nuevasOperaciones[0].fecha = vlrFecha;
+}
+
+console.log(nuevasOperaciones)
+
+// $('boton-agregar').addEventListener('click', () => agregarNuevaOp())
+
+// const agregarNuevaOp = () =>{
+
+// }
+
+
