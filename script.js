4f4891e54087d7a57a005e580deba73455d06dd1
Maria Eugenia O <eugeniaortiz162@gmail.com>
2023-09-04 22:26:56 -0300

    Modificacion de funcion agregar operacion + local storage

--- script.js
+++ script.js
@@ -1,112 +1,80 @@
-const $ = (selector) => document.getElementById(selector);
+// const $ = (selector) => document.getElementById(selector);
+const $ = (selector) => document.querySelector(selector);
+const $$ = (selector) => document.querySelectorAll(selector);
 
 //Boton +nueva operacion
-$('button-operation').addEventListener('click', () => addNewOperation());
+$('#button-operation').addEventListener('click', () => addNewOperation());
 
 const addNewOperation = () =>{
-    $('seccion-balance-y-filtro').classList.add('hidden');
-    $('seccion-operaciones').classList.add('hidden');
-    $('modal-nueva-operacion').classList.remove('hidden');
+    $('#seccion-balance-y-filtro').classList.add('hidden');
+    $('#seccion-operaciones').classList.add('hidden');
+    $('#modal-nueva-operacion').classList.remove('hidden');
 };
 
 //Boton balance
-$('boton-balance').addEventListener('click', () => returnInicio());
+$('#boton-balance').addEventListener('click', () => returnInicio());
 
 const returnInicio = () =>{
-    $('seccion-balance-y-filtro').classList.remove('hidden');
-    $('seccion-operaciones').classList.remove('hidden');
-    $('modal-nueva-operacion').classList.add('hidden');
-    $('seccion-reportes').classList.add('hidden');
+    $('#seccion-balance-y-filtro').classList.remove('hidden');
+    $('#seccion-operaciones').classList.remove('hidden');
+    $('#modal-nueva-operacion').classList.add('hidden');
+    $('#seccion-reportes').classList.add('hidden');
 };
 
 //Boton categorias
-$('boton-categorias').addEventListener('click', () => categoryList());
+$('#boton-categorias').addEventListener('click', () => categoryList());
 
 const categoryList = () =>{
-    $('seccion-balance-y-filtro').classList.add('hidden');
-    $('seccion-operaciones').classList.add('hidden');
-    $('modal-nueva-operacion').classList.add('hidden');
-    $('seccion-categorias').classList.remove('hidden');
+    $('#seccion-balance-y-filtro').classList.add('hidden');
+    $('#seccion-operaciones').classList.add('hidden');
+    $('#modal-nueva-operacion').classList.add('hidden');
+    $('#seccion-categorias').classList.remove('hidden');
 };
 
 //Boton reportes
-$('boton-reportes').addEventListener('click', () => viewReports());
+$('#boton-reportes').addEventListener('click', () => viewReports());
 
 const viewReports = () =>{
-    $('seccion-balance-y-filtro').classList.add('hidden');
-    $('seccion-operaciones').classList.add('hidden');
-    $('modal-nueva-operacion').classList.add('hidden');
-    $('seccion-categorias').classList.add('hidden');
-    $('seccion-reportes').classList.remove('hidden');
-}
-
-//Agregar nuevas operaciones
-
-//array de objetos para pushear ej nuevasOperaciones.descripcion = valor
-$('descripcion').addEventListener('change', () => captarInfo());
-$('monto').addEventListener('change', () => captarInfo());
-$('tipo').addEventListener('change', () => captarInfo());
-$('select-categoria').addEventListener('change', () => captarInfo());
-$('fecha').addEventListener('change', () => captarInfo());
-
-let nuevasOperaciones = {};
-
-const captarInfo = () =>{
-    // deberia setear un id 
-    nuevasOperaciones.Descripcion = $('descripcion').value;
-    nuevasOperaciones.Monto = $('monto').value;
-    nuevasOperaciones.Tipo = $('tipo').value;
-    nuevasOperaciones.Categoria = $('select-categoria').value; //hacer en otra funcion pq tiene valor inicial
-    nuevasOperaciones.Fecha = $('fecha').value;
-    agregarNuevaOp(nuevasOperaciones)
-    
-}
-console.log(nuevasOperaciones)
-
-$('boton-agregar').addEventListener('click', () => retornarAlInicio())
-const retornarAlInicio = () =>{
-    returnInicio();
-    $('imagen-sin-resultado').classList.add('hidden');
-    agregarNuevaOp(nuevasOperaciones);
+    $('#seccion-balance-y-filtro').classList.add('hidden');
+    $('#seccion-operaciones').classList.add('hidden');
+    $('#modal-nueva-operacion').classList.add('hidden');
+    $('#seccion-categorias').classList.add('hidden');
+    $('#seccion-reportes').classList.remove('hidden');
 }
 
 
-const agregarNuevaOp = (nuevasOperaciones) =>{
-    // const operacionDiv = document.createElement('div');
-    // operacionDiv.classList.add('columns', 'is-flex');
 
-    $('contenedor-operaciones').innerHTML = '';
+//LOCAL STORAGE
 
-    for (let prop in nuevasOperaciones){
-        const section = document.createElement('section');
-        section.classList.add('column');
-    
-        const h2 = document.createElement('h2');
-        h2.textContent = prop;
-        
-    
-        const p = document.createElement('p'); //Esto no deberia insertarse asi pq cada elemento tiene un
-        p.classList.add('mt-3');
-        p.textContent = nuevasOperaciones[prop];
-    
-        section.appendChild(h2);
-        section.appendChild(p);
-    
-        $('contenedor-operaciones').appendChild(section);
-    };
+const traerDatos = () => {
+    return JSON.parse(localStorage.getItem("datos"));
 };
 
-//Total ganancias
-console.log($('total-ganancias'))
-
-const totalGanancias = (objOperaciones) =>{
-    $('tipo').addEventListener('change', (e) =>{
-        for(let prop in objOperaciones){
-            if(e.target.value === "Ganancias"){
-                $('total-ganancias').innerHTML = 'hola';
-            }
-        }
-    })
+const subirDatos = (datos) => {
+    localStorage.setItem("datos", JSON.stringify({ ...traerDatos(), ...datos }));
 };
 
-totalGanancias(nuevasOperaciones)
+//ID
+const randomId = () => self.crypto.randomUUID();
+
+//NUEVA OPERACION
+const agregarOperacion = () =>{
+    let operacion = {
+        id: randomId(),
+        descripcion: $('#descripcion').value,
+        monto: $('#monto').value,
+        tipo: $('#tipo').value,
+        categoria: $('#select-categoria').value,
+        fecha: $('#fecha').value
+    }
+    subirDatos(operacion) //SE SUBE AL LOCAL STORAGE
+    console.log(operacion)
+    
+}
+agregarOperacion()
+
+$('#boton-agregar').addEventListener('click', () => agregarOperacion())
+
+
+
+
